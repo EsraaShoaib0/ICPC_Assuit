03fe4431f83418c1e201204097ea49bcca347b79
Esraa El-Shoaiby <133651070+EsraaShoaib0@users.noreply.github.com>
2024-10-22 19:19:28 +0300

    Add files via upload

---
+++ Sheet4_Strings/A.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String w1 = sc.nextLine();
+        String w2 = sc.nextLine();
+        System.out.println(w1.length()+" "+ w2.length());
+        System.out.println(w1+" "+w2);
+
+    }
+}
